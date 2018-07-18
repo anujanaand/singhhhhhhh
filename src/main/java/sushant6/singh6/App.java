233ee0aac7fef89650acf213851aa7498881933a
aujanand <anand.anuj91@gmail.com>
2018-07-18 16:09:30 +0530

    asd

---
+++ src/main/java/sushant6/singh6/App.java
@@ -0,0 +1,13 @@
+package sushant6.singh6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
